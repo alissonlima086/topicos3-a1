517806a4f02c96124e3de8af01e97ab1a86bd543
alissonlima086 <alissonlima086@gmail.com>
2026-03-30 09:53:19 -0300

    delete: removendo migração inicial do entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e  
 
 echo "Aguardando SQL Server ficar pronto..."
-sleep 15 # não o ideal, mas po aga é isso, arrumar o healthcheck depois
+sleep 15 # não o ideal, mas por agora funciona, arrumar o healthcheck depois
 
 cd /app/src
 
@@ -11,17 +11,6 @@ dotnet restore
 
 echo "Buildando aplicação..."
 dotnet build
-
-# tenta criar a migration, no momento já deve existir, mas caso não exista, criar.
-echo "Criando migration Initial..."
-if dotnet ef migrations list | grep -q "Initial"; then
-    echo "Migration 'Initial' já existe, pulando..."
-else
-    dotnet ef migrations add Initial -o Data/Migrations || {
-        echo "ERRO: Falha ao criar migrations. Verifique se DbContext existe!"
-        exit 1
-    }
-fi
 
 echo "Atualizando banco de dados..."
 dotnet ef database update || {
